901c5febe1f49c09299b08e8252e501e0ec4ef69
Cameron Waeland <cameron.waeland@compass.com>
2020-08-20 17:33:12 -0400

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ DESCRIPTION = "Generate Bazel BUILD files for a Python project."
 
 setup(
     name='pazel',
-    version='0.1.0',
+    version='0.2.0',
     description=DESCRIPTION,
     packages=find_packages(exclude=('sample_app', 'sample_app.foo', 'sample_app.tests')),
     entry_points={
